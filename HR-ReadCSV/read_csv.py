70ee0c79e3c029c8973f6279483a67e2da198bc9
JosChiroyAifan <3049004510116@ingenieria.usac.edu.gt>
2019-10-19 17:30:14 -0600

    HR-19.10.2019 17:30

--- HR-ReadCSV/read_csv.py
+++ HR-ReadCSV/read_csv.py
@@ -1,5 +1,6 @@
 
 import pandas
+import impyute as impy
 from pandas.plotting import scatter_matrix
 import matplotlib.pyplot as plt
 from sklearn import model_selection
@@ -12,8 +13,9 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
-url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
-names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+url = "/home/port-sagp2705/Escritorio/Fireball_And_Bolide_Reports.csv"
+names = ['a','b','c','d','e','f','g','h','i','j']
 dataset = pandas.read_csv(url, names=names)
-dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
-plt.savefig("mygraph.png")
+#print(impy.mean(dataset))
+print(dataset.head(20))
+print(dataset.describe())
