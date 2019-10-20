c68608f44d090be71a2a692e3f3d4cd58776a8b6
Jose_Wannan <49357066+proyectoswannan@users.noreply.github.com>
2019-10-19 18:49:44 -0600

    inicio,/pagina/

--- Space_FireFly/FireFly/urls.py
+++ Space_FireFly/FireFly/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='Inicio'),
+    path('Inicio/', views.index, name='Inicio'),
+    path('Upload/',views.entrada, name='Upload'),
 ]
