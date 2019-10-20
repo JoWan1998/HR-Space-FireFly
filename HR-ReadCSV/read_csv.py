a19ed21bee7305a297fe061822aad48972f99e42
JosChiroyAifan <3049004510116@ingenieria.usac.edu.gt>
2019-10-20 07:43:10 -0600

    HR-20.10.2019 07:43

--- HR-ReadCSV/read_csv.py
+++ HR-ReadCSV/read_csv.py
@@ -1,31 +1,20 @@
-
 import pandas as pd
+import numpy as np
 import csv
-import impyute as impy
-from pandas.plotting import scatter_matrix
 import matplotlib.pyplot as plt
-from sklearn import model_selection
-from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
-from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import SVC
-url = "/home/port-sagp2705/Escritorio/Fireball_And_Bolide_Reports.csv"
+from sklearn.impute import SimpleImputer
 
+#Importing Dataset
+dataset = pd.read_csv('/home/port-sagp2705/Escritorio/Fireball_And_Bolide_Reports.csv')
 
-
-
-
-
-with open('/home/port-sagp2705/Escritorio/Fireball_And_Bolide_Reports.csv', newline='') as csvfile:
-    data = list(csv.reader(csvfile))
-
-
-    print(data)
-
-    complete_data = impy(data)
-    print(complete_data)
+#Missing Data
+imr = SimpleImputer(missing_values=np.nan, strategy='median')
+imr = imr.fit(dataset[['Velocity (km/s)']])
+dataset['Velocity (km/s)'] = imr.transform(dataset[['Velocity (km/s)']]).ravel()
+imr = imr.fit(dataset[['Velocity Components (km/s): vx']])
+dataset['Velocity Components (km/s): vx'] = imr.transform(dataset[['Velocity Components (km/s): vx']]).ravel()
+imr = imr.fit(dataset[['Altitude (km)']])
+dataset['Altitude (km)'] = imr.transform(dataset[['Altitude (km)']]).ravel()
+print("---")
+print(dataset)
+dataset.to_csv('modificado.csv')
