38d41c8a82a69f95e0888971ead18cc14c43d47f
Jose_Wannan <49357066+proyectoswannan@users.noreply.github.com>
2019-10-19 18:04:13 -0600

    Web APP_ Inicio Integracion

--- Space_FireFly/FireFly/models.py
+++ Space_FireFly/FireFly/models.py
@@ -2,4 +2,12 @@ from csvImporter.model import CsvModel
 from django.db import models
 #bd
 # Create your models here.
+class data(models.Model):
+    nombre = models.CharField()
+    file = models.FileField()
+
+    def __str__(self):
+        return self.nombre
+
+
 
